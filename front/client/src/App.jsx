53897810290e31679640060edea8d2f5f6421d74
urosperisic <uros2907@gmail.com>
2025-11-24 23:51:40 +0100

    Add base style 3

--- front/client/src/App.jsx
+++ front/client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Specials from './pages/Specials';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           </Routes>
         </main>
       </div>
+      <Footer />
     </Router>
   );
 }
---
+++ front/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Copyright © 2025-present, Mladen - Uros. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
